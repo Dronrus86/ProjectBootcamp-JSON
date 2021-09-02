c08403e1c4e228790a816dcf100b00fa20078ce9
Dronrus86 <pop4666@yandex.ru>
2021-09-02 21:48:43 +0300

    json

--- style.js
+++ style.js
@@ -1,14 +1,9 @@
-const currencyButton = document.getElementsByClassName('.currencyButton');    //Кнопка "У меня есть"
+const currencyButton = document.querySelectorAll('.currencyButton');    //Кнопка "У меня есть"
 const changeButton = document.getElementsByClassName('.changeButton');       //Кнопка  "Хочу купить"
 const heveInput = document.querySelector('.heveInput');            //Инпут "У меня есть"
 const changeInput = document.querySelector('.changeInput');       //Инпут "Хочу купить"
 const translationHave = document.querySelector('.translation_have');      //  p - хочу купить 
 const translationChange = document.querySelector('.translation_change'); // р- у меняесть 
-
-
-
-
-
 
 
 
@@ -28,16 +23,30 @@ async function getCur(){
 
 const translationHave = document.querySelector('.currencyButtonActiv');      //  кнопка активная 'у меня есть' 
 const translationChange = document.querySelector('.changeButtonActiv');      // кнпка активная 'хочу купить.' 
-changeInput.value = (heveInput.value*ret[translationHave.value] / ret[translationChange.value]).toFixed(4);
+changeInput.value = (heveInput.value*ret[translationChange.value] / ret[translationHave.value] ).toFixed(4);
     
 }
 
 
-window.onload = function (){
-    let changeButton = document.getElementsByClassName('.changeButton'); 
-    for(let i = 0; i < changeButton.length; i++)
-    changeButton[i].onclick = function (event){
-             console.log(changeButton);
-               
-           }
+window.onload = function() {
+    function strPad() {
+      console.log(this.value);
+    }
+    for (let i = 0; i < currencyButton.length; i++) {
+        currencyButton[i].onclick = strPad,
+        function(event) {
+        const translationHave = document.querySelector('.currencyButtonActiv');  //  кнопка активная 'у меня есть' 
+        const translationChange = document.querySelector('.changeButtonActiv');  
+        event.target.classList.toggle('currencyButtonActiv');    // кнпка активная 'хочу купить.' 
+        changeInput.value = (heveInput.value*ret[translationChange.value] / ret[event.target.value ]).toFixed(4);
+    }
+  }
+
+  heveInput.addEventListener('input', function() {
+    const translationHave = document.querySelector('.currencyButtonActiv');      //  кнопка активная 'у меня есть' 
+    const translationChange = document.querySelector('.changeButtonActiv');  
+    changeInput.value = (heveInput.value*ret[translationChange.value] / ret[translationHave.value] ).toFixed(4);
+})
 }
+
+
